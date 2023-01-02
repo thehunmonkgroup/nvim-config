216dc35c15d6b75da8b77eeb6fbd6e6199073209
Chad Phillips <chad@apartmentlines.com>
2023-01-01 20:24:25 -0500

    spacing

--- lua/user/util.lua
+++ lua/user/util.lua
@@ -1,11 +1,11 @@
 local _M = {}
 
 function _M.map(mode, lhs, rhs, opts)
-    local options = { noremap = true }
-    if opts then
-        options = vim.tbl_extend("force", options, opts)
-    end
-    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+  local options = { noremap = true }
+  if opts then
+    options = vim.tbl_extend("force", options, opts)
+  end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
 function _M.nmap(lhs, rhs, opts)
@@ -13,9 +13,9 @@ function _M.nmap(lhs, rhs, opts)
 end
 
 function _M.get_mason_package_install_path(package_name)
-    local mason_registry = require("mason-registry")
-    local package = mason_registry.get_package(package_name)
-    return package:get_install_path()
+  local mason_registry = require("mason-registry")
+  local package = mason_registry.get_package(package_name)
+  return package:get_install_path()
 end
 
 return _M
