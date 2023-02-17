aefe4df19dd7604078e10c413857fb294f615e8f
Chad Phillips <chad@apartmentlines.com>
2023-02-16 21:22:19 -0500

    move mappings to separate file, disable visual block mappings for > and <

---
+++ lua/user/mappings.lua
@@ -0,0 +1,30 @@
+-- Mapping data with "desc" stored directly by vim.keymap.set().
+--
+-- Please use this mappings table to set keyboard mapping since this is the
+-- lower level configuration and more robust one. (which-key will
+-- automatically pick-up stored data by this setting.)
+return {
+  -- first key is the mode
+  n = {
+    -- second key is the lefthand side of the map
+    -- mappings seen under group name "Buffer"
+    ["<leader>bb"] = { "<cmd>tabnew<cr>", desc = "New tab" },
+    ["<leader>bc"] = { "<cmd>BufferLinePickClose<cr>", desc = "Pick to close" },
+    ["<leader>bj"] = { "<cmd>BufferLinePick<cr>", desc = "Pick to jump" },
+    ["<leader>bt"] = { "<cmd>BufferLineSortByTabs<cr>", desc = "Sort by tabs" },
+    ["X"] = { "<cmd>Neotree toggle<cr>" , desc = "Toggle Neotree" },
+    ["<Leader>z"] = { "<cmd>Neotree source=filesystem<cr>" , desc = "Neotree filesystem tab" },
+    ["<Leader>a"] = { "<cmd>Neotree source=buffers<cr>" , desc = "Neotree buffers tab" },
+    ["<leader><leader>"] = { "<cmd>Telescope buffers<cr>" , desc = "Telescope buffers" },
+    -- quick save
+    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+  },
+  v = {
+    ["<"] = false,
+    [">"] = false,
+  },
+  t = {
+    -- setting a mapping to false will disable it
+    -- ["<esc>"] = false,
+  },
+}
